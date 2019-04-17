a7dbffc0c7df802b6d2e4e60b0fb538a07135889
Sergei Glimis <sglimis@crefeld.org>
2019-04-17 08:47:45 -0400

    adding more keywords

--- Junk_Voice_Mail.py
+++ Junk_Voice_Mail.py
@@ -2,26 +2,17 @@ import speech_recognition as sr
 from difflib import SequenceMatcher
 
 Spam_Keywords = (".org",".com","we","register","we need","dollars",
-                 "help us","irs","this is","interested","donate","donation")
+                 "help us","irs","this is","interested","donate",
+                 "donation","information","insurance","social security",
+                 "")
 # i will also decipher in a later version of the call is from a known contact or not.
 Non_Spam_Keywords = ("love you","i'll","see you","hey man","sup man","what's up",
-                     "it's me","later","home"," i ", "honey","mom","dad")
-
-Example_Spam = ["hello this is june with infocus we are looking for people to help us fund our service"
-                ,"hi there, this is bob with idso we need you to register with us to help us get trump elected"
-                ,"Hi i'm hanna and im with bic we're looking for people who are interested in pens",
-                "this is the IRS you have been audited Call us at",
-                "Hello I'm calling with doornob inc. we are looking for people to register for our service",
-                "hey there this is john with temple we are looking for people to help us fund a new program",
-                "hello my name is dean, i'm calling with target we are looking for donations"]
-
-Example_Non_Spam = ["Hey man it's john, thought i'd call and see how your doing, I'll see you on monday bye",
-                    "hey honey it's jim couldn't remember which ice cream you wanted me to buy get back to me as soon as you can love you bye",
-                    "Hey sam it's kim i'm gonna need you you to work late on friday, get back to me let me know if that works for you bye",
-                    "hey dad it's me just wanted to let you know im coming out for fathers dad, get back to me as soon as you can i love you bye."]
+                     "it's me","later","home"," i ", "honey","mom","dad","dude",
+                     "bro","love","last week","i'll see you","wanted to see"
+                     "shopping","supermarket","kids","thanks")
 
 
-DEBUGGING = False
+DEBUGGING = True
 def debug_print(text):
     # this allows you to turn off the printing of things that aren't needed undless it's by the developer 
     global DEBUGGING
