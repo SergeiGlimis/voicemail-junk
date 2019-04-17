3c387d9d3ab65255b66154ae170894361c596dc3
Sergei Glimis <sglimis@crefeld.org>
2019-04-17 09:25:42 -0400

    added more keywords
    doubled the amount of keywords

--- Junk_Voice_Mail.py
+++ Junk_Voice_Mail.py
@@ -4,12 +4,17 @@ from difflib import SequenceMatcher
 Spam_Keywords = (".org",".com","we","register","we need","dollars",
                  "help us","irs","this is","interested","donate",
                  "donation","information","insurance","social security",
-                 "")
+                 "sweepstakes","help us","cash","money","price","won","winning"
+                 ,"successful","Marketing","business","retire","income","incrute"
+                 ,"online","give us","elect","trump")
+
 # i will also decipher in a later version of the call is from a known contact or not.
 Non_Spam_Keywords = ("love you","i'll","see you","hey man","sup man","what's up",
                      "it's me","later","home"," i ", "honey","mom","dad","dude",
                      "bro","love","last week","i'll see you","wanted to see"
-                     "shopping","supermarket","kids","thanks")
+                     "shopping","supermarket","kids","thanks","we'll see you",
+                     "hey dude","appointment","office","remember","bring","wait",
+                     "my","me","outside","walking","driving")
 
 
 DEBUGGING = True
@@ -19,7 +24,9 @@ def debug_print(text):
     if DEBUGGING:
         print(text)
 
-def Decipher_Spam(File):
+
+
+def VoiceWav(File):
     #takes an arguent which is the file 
     debug_print("Starting to decipher.") # uses the debug print
     VM_Audio_File = sr.AudioFile(str(File)) # uses the file that was an argument and makes it a file recognizable to speech recognition.
@@ -29,7 +36,14 @@ def Decipher_Spam(File):
     with VM_Audio_File as source: # uses that file we gave to speechRecognition 
         audio = r.record(source) # play the audio file under the hood and recognise the speech, this text will be put to "audio".
     debug_print("recognize audio")
-    value = r.recognize_google(audio).lower() # names it value now and makes it lower cased to avoid inconsistancies.
+    value = r.recognize_google(audio).lower()
+    return value
+
+
+
+    
+def DecSpam(value):
+ # names it value now and makes it lower cased to avoid inconsistancies.
     debug_print(value)
     value2 = str(value).split() # split the text from the file by word.
     debug_print("spliting ")
@@ -42,6 +56,7 @@ def Decipher_Spam(File):
             spam = spam+1
 
         elif word in Non_Spam_Keywords: # if it's in the non spam list do something
+            debug_print(word)
             non_spam = non_spam +1
 
     if spam > non_spam:
